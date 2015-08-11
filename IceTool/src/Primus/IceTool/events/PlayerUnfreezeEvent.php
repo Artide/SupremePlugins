509f5ef7614f2dd442e944645ca3116047a667c0
PrimusLV <k.drivnieks@inbox.lv>
2015-08-11 19:04:24 +0300

    Update PlayerUnfreezeEvent.php

--- IceTool/src/Primus/IceTool/events/PlayerUnfreezeEvent.php
+++ IceTool/src/Primus/IceTool/events/PlayerUnfreezeEvent.php
@@ -1,1 +1,28 @@
+<?php
+namespace Primus\IceTool\events;
 
+use pocketmine\event\player\PlayerEvent;
+use pocketmine\Player;
+use Primus\IceTool\Main;
+
+class PlayerUnfreezeEvent extends PlayerEvent{
+	
+	public static $eventPool = array();
+	public static $nextEvent = 0;
+	public static $handlerList = null;
+	
+	protected $player;
+	protected $eventName = null;
+	
+	public function __construct(Player $player){
+		$this->player = $player;
+	}
+	
+	public function getPlayer(){
+		return $this->player;
+	}
+	
+	public function getHandlers(){
+		return $this->handlerList;
+}
+} 
