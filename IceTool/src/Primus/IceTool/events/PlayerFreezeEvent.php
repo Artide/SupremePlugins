6d0c9654f924d935c00e30e334f37c462e2febec
PrimusLV <k.drivnieks@inbox.lv>
2015-08-11 19:03:21 +0300

    Update PlayerFreezeEvent.php

--- IceTool/src/Primus/IceTool/events/PlayerFreezeEvent.php
+++ IceTool/src/Primus/IceTool/events/PlayerFreezeEvent.php
@@ -1,1 +1,36 @@
 
+ <?php
+namespace Primus\IceTool\events;
+
+use pocketmine\event\player\PlayerEvent;
+use pocketmine\Player;
+use Primus\IcyManager\Main;
+
+class PlayerFreezeEvent extends PlayerEvent{
+	
+	public static $handlerList = null;
+	public static $nextEvent = 0;
+	public static $eventPool = array();
+	
+	protected $eventName = null;
+	protected $player;
+	protected $time; // TODO
+	protected $isFrozen = true;
+	
+	public function __construct(Player $player, $freeze=true){
+		$this->player = $player;
+		$this->isFrozen = $freeze;
+	}
+	
+	public function getPlayer(){
+		return $this->player;
+	}
+	
+	public function isFrozen(){
+		return $this->isFrozen;
+	}
+	
+	public function getHandlers(){
+		return $this::$handlerList;
+	}
+}
